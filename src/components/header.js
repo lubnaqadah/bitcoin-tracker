c84f9d2161e7a66ffacaeed5e271b531d0122a84
lubna qadah <lubna.alqadah@gmail.com>
2018-03-22 22:26:29 -0700

    added header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class header extends React.Component{
+	render(){
+		return(
+			<h1>Bitcoin Tracker</h1>
+			<h3>Track your bitcoin net worth</h3>
+		
+		
+		)
+	}
+}
+
+export default header;
